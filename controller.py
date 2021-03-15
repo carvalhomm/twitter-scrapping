e61e9183f5d051ab358b9ab8c648196a9985e8fa
Matheus Carvalho <matheus.c.g.moreira@gmail.com>
2021-03-15 08:15:30 -0300

    corrections

--- controller.py
+++ controller.py
@@ -1,9 +1,12 @@
 import core as WebBrowserCore
 
 class Controller:
-  def __init__(self, browser_name):
-    self.browser = WebBrowserCore(browser_name)
+  def __init__(self, browser_name, headless):
+    self.browser = WebBrowserCore.WebDriverCore(browser_name, headless)
 
-  def search_for(self, url = '', input):
+  def search_for(self, url = '', text = ''):
     self.browser.open_url(url)
-    self.browser
+    self.browser.wait_element_visible('#search_query')
+    element = self.browser.find_element_by('ID', 'search_query')
+    self.browser.write_on_element(element, text)
+    self.browser.press_key(element, 'ENTER')
--- core.py
+++ core.py
@@ -3,16 +3,22 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 class WebDriverCore:
-  def __init__(self, browser_name):
-    self.driver = self.create_webdriver(browser_name)
+  def __init__(self, browser_name, headless):
+    self.driver = self.create_webdriver(browser_name, headless)
     self.wait = WebDriverWait(self.driver, 10)
 
-  def create_webdriver(self, browser_name = 'chrome'):
+  def create_webdriver(self, browser_name = 'chrome', headless = False):
     try:
       if browser_name == 'chrome':
-        return webdriver.Chrome()
+        chrome_options = Options()
+        if headless is True:
+          chrome_options.add_argument('--headless')
+          chrome_options.add_argument('--no-sandbox')
+          chrome_options.add_argument('--disable-dev-shm-usage')
+        return webdriver.Chrome(chrome_options=chrome_options, executable_path='./chromedriver.exe')
       elif browser_name == 'firefox':
         return webdriver.Firefox()
       elif browser_name == 'edge':
@@ -22,31 +28,28 @@ class WebDriverCore:
       print(error)
       return None
 
-  def wait_text_element(self, method = 'ID', selector, value):
+  def wait_text_element(self, selector = None, value = None):
     if self.wait is None or value is None or selector is None:
       return None
-    method = method.upper()
-    self.wait.until(EC.text_to_be_present_in_element((getattr(By, method), selector), value))
+    self.wait.until(EC.text_to_be_present_in_element(selector, value))
 
-  def wait_element_invisible(self, method = 'ID', value):
+  def wait_element_invisible(self, value = None):
     if self.wait is None or value is None:
       return None
-    method = method.upper()
-    self.wait.until(EC.invisibility_of_element_located(getattr(By, method), value))
+    self.wait.until(EC.invisibility_of_element_located(value))
 
-  def wait_element_visible(self, method = 'ID', value):
+  def wait_element_visible(self, value = None):
     if self.wait is None or value is None:
       return None
-    method = method.upper()
-    self.wait.until(EC.visibility_of(getattr(By, method), value))
+    self.wait.until(EC.visibility_of_element_located(value))
 
-  def find_element_by(self, method = 'ID', value):
+  def find_element_by(self, method = 'ID', value = None):
     if self.driver is None or value is None:
       return None
     method = method.upper()
     return self.driver.find_element(getattr(By, method), value)
 
-  def find_elements_by(self, method = 'ID', value):
+  def find_elements_by(self, method = 'ID', value = None):
     if self.driver is None or value is None:
       return None
     method = method.upper()
@@ -57,12 +60,12 @@ class WebDriverCore:
       return
     element.click()
 
-  def press_key(self, element, key='ENTER'):
+  def press_key(self, element = None, key = 'ENTER'):
     if self.driver is None or element is None:
       return
     element.send_keys(getattr(Keys, key))
   
-  def write_on_element(self, element, value = ''):
+  def write_on_element(self, element = None, value = ''):
     if self.driver is None or element is None:
       return
     element.send_keys(value)
