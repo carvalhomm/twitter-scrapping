c9d16f07d911f5497d14b09514aea2be153eff71
Matheus Moreira <matheus.c.g.moreira@gmail.com>
2021-05-06 13:32:16 -0300

    more things

--- user_interface.py
+++ user_interface.py
@@ -5,13 +5,16 @@ class UserInterface:
   def __init__():
     self.window = None
     self.layout = None
-    self.controller = controller.Controller('chrome', False)
+    self.controller = None
 
   def generate_layout(self):
     self.layout = [
-      [sg.Text('Twitter Scrapping', size=(40, 1))],
+      [sg.Text('Twitter Scrapping', justification='center', size=(40, 1))],
       [
-        sg.Text('Quantidade de resultados (O limite é 30)', size=(40, 1)),
+        sg.Checkbox('Deseja ver a interação com o browser no site do Twitter?', default=False, key='BROWSER_HEADLESS')
+      ]
+      [
+        sg.Text('Limite de Resultados (O limite é 30)', size=(40, 1)),
         sg.Multiline(size=(70, 5), enter_submits=False, key='QUANTIDADE', do_not_clear=False)
       ]
       [sg.Text('Pesquisa por Trend Topics', size=(40, 1))],
@@ -32,7 +35,13 @@ class UserInterface:
     ]
 
   def draw_window(self, title = 'Twitter Scrapping'):
-    self.window = sg.Window(title, layout=self.layout)
+    self.window = sg.Window(title, layout=self.layout, default_button_element_size=(8,2), use_default_focus=False)
+
+  def instance_browser(self, headless = False):
+    self.controller = controller.Controller('chrome', headless)
+
+  def interact_with_browser(self):
+    self.controller.open_url('https://twitter.com/explore')
 
   def wait_for_user_interactions(self):
     user_interacting = True
@@ -42,8 +51,16 @@ class UserInterface:
       print('texto --> ', values)
       if event in (sg.WIN_CLOSED, 'EXIT'):
         user_interacting = False
+        continue
+      quantidade = int(values['QUANTIDADE'].rstrip())
+      browser_headless = bool(values['BROWSER_HEADLESS'])
+      self.instance_browser(browser_headless)
+      self.interact_with_browser()
+      if event == 'Pesquisa por Trend Topics':
+        trends = str(values['TREND_TOPICS'].rstrip()).trim()
+      if event == 'Pesquisar por Hashtags':
+        hashtags = str(values['HASHTAGS'].rstrip()).trim()
+      if event == 'Pesquisar por Palavras Chave':
+        palavrasChave = str(values['PALAVRAS_CHAVE'].rstrip()).trim()
 
     self.window.close()
-
-  def interact_with_browser(self):
-    self.controller.open_url('https://twitter.com/explore')
